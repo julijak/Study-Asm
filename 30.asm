8e9d486d2d461028c09975d5f8cf7a3e6d448719
Yuliya <yuliya.kul@itechart-group.com>
2015-01-01 18:11:30 +0300

    Added 30

---
+++ 30.asm
@@ -0,0 +1,15 @@
+.386 
+.model flat, stdcall 
+ExitProcess PROTO      ,:DWORD 
+MessageBoxA PROTO      ,:DWORD, :DWORD, :DWORD, :DWORD 
+.data 
+   MsgBoxCaption  db "Заголовок",0 
+   MsgBoxText     db "Текст!",0 
+.const 
+   NULL        equ  0 
+   MB_OK    equ  0
+.code 
+   Main: 
+    INVOKE    MessageBoxA, NULL, ADDR MsgBoxText, ADDR MsgBoxCaption, MB_OK 
+    INVOKE    ExitProcess, NULL 
+   end Main
